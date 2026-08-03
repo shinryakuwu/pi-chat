d0da30c7dbbb5f40fbd9b22ffed4b9f4af533405
l0ser <yourname@example.com>
2026-08-04 02:00:41 +0300

    add user model tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be valid with valid attributes" do
+    user = User.new(username: "computer_angel", password: "benotafraid")
+    assert user.valid?
+  end
+
+  test "should be invalid without username" do
+    user = User.new(password: "benotafraid")
+    assert_not user.valid?
+    assert_includes user.errors[:username], "can't be blank"
+  end
+
+  test "should be invalid without password" do
+    user = User.new(username: "computer_angel")
+    assert_not user.valid?
+    assert_includes user.errors[:password], "can't be blank"
+  end
 end
