a239de1f61374693151fe25182306c0352b656b5
l0ser <yourname@example.com>
2026-09-14 14:00:46 +0300

    select chat on reload

--- app/javascript/controllers/sidebar_controller.js
+++ app/javascript/controllers/sidebar_controller.js
@@ -1,6 +1,25 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  selectOnReload(event) {
+    if (event.target.id !== "navigation") {
+      return
+    }
+
+    const chatContent = this.element.querySelector("#chat_content")
+    const currentChatId = chatContent?.dataset.chatId
+
+    if (!currentChatId) {
+      return
+    }
+
+    const currentChat = this.element.querySelector(
+      `.nav_item[data-chat-id="${currentChatId}"]`
+    )
+
+    currentChat?.classList.add("active")
+  }
+
   select(event) {
     this.clear()
     event.currentTarget.classList.add("active")
