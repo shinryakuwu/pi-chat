b0686cd818d49b75205be8d1db9d47c49c1e627e
l0ser <yourname@example.com>
2026-09-20 18:45:27 +0300

    chat websockets

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,7 +7,7 @@ module UsersHelper
         "empty.png"
       end
 
-    image_tag image,
+    image_tag rails_blob_path(image, only_path: true),
       width: width,
       height: height,
       class: class_attr,
